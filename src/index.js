b96a4134f81496b10e4f38cc064abeac9f9d3bb3
AdrielModollo <adrielmodollo@outlook.com>
2022-09-29 21:02:10 -0300

    feat: add lauch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const pup = require('puppeteer');
 
 const url = "https://dicionariocriativo.com.br/";
-const searchWord = "comida"
+const searchWord = "comida";
+
+(async () => {
+    const browser = await pup.launch({ headless: false }); //inicializa o nosso navegador, headless desabilita o funcionamento oculto.
+})();
